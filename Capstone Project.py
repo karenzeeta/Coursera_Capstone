350cd08a1e4f1c91c424764ffb3014ffa147d28f
karenzeeta <64039127+karenzeeta@users.noreply.github.com>
2020-05-31 22:38:47 +0100

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Karen's Capstone Project Notebook
+# ### This notebook will be mainly used for my capstone project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:print("Hello Capstoners!")
+
+
+
+
+
+
